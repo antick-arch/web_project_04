c1a5d5f8785ed00e113aa5de244b0fe1e97ba473
Antick Chandra Kuri <156878911+antick-arch@users.noreply.github.com>
2024-12-06 00:32:57 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function animation(){
+    var at = gsap.timeline()
+
+at.to("#back1",{
+    top: "-100%",
+    delay:0.5,
+    duration:0.5,
+    ease:"expo.out"
+})
+at.from("#back2",{
+    top: "100%",
+    delay:1,
+    duration:0.5,
+    ease:"expo.out"
+},"same")
+at.to("#back-ward h1",{
+    delay:0.6,
+    color:"black"
+},"same")
+at.to("#back-ward",{
+    display:"none"
+})
+}
+animation();
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#container'),
+    smooth: true
+});
